45f484ac67eaf41d3325c2ffcc16764bab42a531
ImaKrp <juliocargoncalves@gmail.com>
2024-05-25 00:22:34 -0300

    random

--- java/com/ciscos/project/frames/CharCreation.java
+++ java/com/ciscos/project/frames/CharCreation.java
@@ -8,7 +8,11 @@ import com.ciscos.project.Character;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JOptionPane;
+import javax.swing.border.EmptyBorder;
 
 public class CharCreation extends javax.swing.JFrame {
 
@@ -16,12 +20,13 @@ public class CharCreation extends javax.swing.JFrame {
         initComponents();
         Context.setCreation(this);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        cbClass.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));        
+        cbClass.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         jButton1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         int w = this.getSize().width;
         int h = this.getSize().height;
-        int x = (dim.width-w)/2 + 50;
-        int y = (dim.height-h)/2 + 50;
+        
+        int x = (dim.width - w) / 2 + 50;
+        int y = (dim.height - h) / 2 + 50;
 
         this.setLocation(x, y);
     }
@@ -62,153 +67,200 @@ public class CharCreation extends javax.swing.JFrame {
         getContentPane().add(specialField, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 260, -1, -1));
 
         specialInput.setEditable(false);
+        specialInput.setBackground(new java.awt.Color(40, 40, 40));
+        specialInput.setForeground(new java.awt.Color(221, 221, 221));
         specialInput.setText("5");
         specialInput.setActionCommand("<Not Set>");
+        specialInput.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         specialInput.setCaretColor(new java.awt.Color(238, 238, 238));
-        specialInput.addCaretListener(new javax.swing.event.CaretListener() {
-            public void caretUpdate(javax.swing.event.CaretEvent evt) {
-                specialInputCaretUpdate(evt);
-            }
-        });
-        getContentPane().add(specialInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 280, 160, 30));
+        specialInput.setDisabledTextColor(new java.awt.Color(221, 221, 221));
+        specialInput.setBorder(BorderFactory.createCompoundBorder(
+            specialInput.getBorder(),
+            BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+    specialInput.setEnabled(false);
+    specialInput.addCaretListener(new javax.swing.event.CaretListener() {
+        public void caretUpdate(javax.swing.event.CaretEvent evt) {
+            specialInputCaretUpdate(evt);
+        }
+    });
+    getContentPane().add(specialInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 280, 160, 30));
 
-        speedInput.setEditable(false);
-        speedInput.setText("15");
-        speedInput.setActionCommand("<Not Set>");
-        speedInput.setCaretColor(new java.awt.Color(238, 238, 238));
-        speedInput.addCaretListener(new javax.swing.event.CaretListener() {
-            public void caretUpdate(javax.swing.event.CaretEvent evt) {
-                speedInputCaretUpdate(evt);
-            }
-        });
-        getContentPane().add(speedInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 280, 160, 30));
+    speedInput.setEditable(false);
+    speedInput.setBackground(new java.awt.Color(40, 40, 40));
+    speedInput.setForeground(new java.awt.Color(221, 221, 221));
+    speedInput.setText("15");
+    speedInput.setActionCommand("<Not Set>");
+    speedInput.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+    speedInput.setCaretColor(new java.awt.Color(40, 40, 40));
+    speedInput.setDisabledTextColor(new java.awt.Color(221, 221, 221));
+    speedInput.setEnabled(false);
+    speedInput.setFocusable(false);
+    speedInput.setPreferredSize(new java.awt.Dimension(19, 19));
+    speedInput.addCaretListener(new javax.swing.event.CaretListener() {
+        public void caretUpdate(javax.swing.event.CaretEvent evt) {
+            speedInputCaretUpdate(evt);
+        }
+    });
+    getContentPane().add(speedInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 280, 160, 30));
+    speedInput.setBorder(BorderFactory.createCompoundBorder(
+        speedInput.getBorder(),
+        BorderFactory.createEmptyBorder(5, 5, 5, 5)));
 
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("Velocidade");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 260, -1, -1));
+jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+jLabel5.setText("Velocidade");
+getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 260, -1, -1));
 
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("Pontos de vida (HP)");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, -1, -1));
+jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+jLabel4.setText("Pontos de vida (HP)");
+getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, -1, -1));
 
-        hpInput.setEditable(false);
-        hpInput.setText("60");
-        hpInput.setActionCommand("<Not Set>");
-        hpInput.setCaretColor(new java.awt.Color(238, 238, 238));
-        hpInput.addCaretListener(new javax.swing.event.CaretListener() {
-            public void caretUpdate(javax.swing.event.CaretEvent evt) {
-                hpInputCaretUpdate(evt);
-            }
-        });
-        hpInput.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                hpInputActionPerformed(evt);
-            }
-        });
-        getContentPane().add(hpInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 280, 160, 30));
+hpInput.setEditable(false);
+hpInput.setBackground(new java.awt.Color(40, 40, 40));
+hpInput.setForeground(new java.awt.Color(221, 221, 221));
+hpInput.setText("60");
+hpInput.setActionCommand("<Not Set>");
+hpInput.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+hpInput.setCaretColor(new java.awt.Color(40, 40, 40));
+hpInput.setBorder(BorderFactory.createCompoundBorder(
+    hpInput.getBorder(),
+    BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+    hpInput.setDisabledTextColor(new java.awt.Color(221, 221, 221));
+    hpInput.setEnabled(false);
+    hpInput.setMargin(new java.awt.Insets(20, 20, 20, 20));
+    hpInput.addCaretListener(new javax.swing.event.CaretListener() {
+        public void caretUpdate(javax.swing.event.CaretEvent evt) {
+            hpInputCaretUpdate(evt);
+        }
+    });
+    hpInput.addActionListener(new java.awt.event.ActionListener() {
+        public void actionPerformed(java.awt.event.ActionEvent evt) {
+            hpInputActionPerformed(evt);
+        }
+    });
+    getContentPane().add(hpInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 280, 160, 30));
 
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Nome do Personagem");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, -1));
+    jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+    jLabel2.setText("Nome do Personagem");
+    getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, -1));
 
-        nameInput.setActionCommand("<Not Set>");
-        nameInput.addCaretListener(new javax.swing.event.CaretListener() {
-            public void caretUpdate(javax.swing.event.CaretEvent evt) {
-                nameInputCaretUpdate(evt);
-            }
-        });
-        getContentPane().add(nameInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, 270, 30));
+    nameInput.setBackground(new java.awt.Color(40, 40, 40));
+    nameInput.setForeground(new java.awt.Color(221, 221, 221));
+    nameInput.setActionCommand("<Not Set>");
+    nameInput.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+    nameInput.setBorder(BorderFactory.createCompoundBorder(
+        nameInput.getBorder(),
+        BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+nameInput.addCaretListener(new javax.swing.event.CaretListener() {
+    public void caretUpdate(javax.swing.event.CaretEvent evt) {
+        nameInputCaretUpdate(evt);
+    }
+    });
+    getContentPane().add(nameInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 60, 270, 30));
 
-        cbClass.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Arqueiro", "Berserker", "Mago" }));
-        cbClass.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cbClassActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cbClass, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, 270, 30));
+    cbClass.setBackground(new java.awt.Color(40, 40, 40));
+    cbClass.setForeground(new java.awt.Color(221, 221, 221));
+    cbClass.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Arqueiro", "Berserker", "Mago" }));
+    cbClass.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+    cbClass.setOpaque(true);
+    cbClass.addActionListener(new java.awt.event.ActionListener() {
+        public void actionPerformed(java.awt.event.ActionEvent evt) {
+            cbClassActionPerformed(evt);
+        }
+    });
+    getContentPane().add(cbClass, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, 270, 30));
+    for (int i = 0; i < cbClass.getComponentCount(); i++)
+    {
+        if (cbClass.getComponent(i) instanceof JComponent) {
+            ((JComponent) cbClass.getComponent(i)).setBorder(new EmptyBorder(0, 0,0,0));
+        }
 
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("Classe do Personagem");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
+        if (cbClass.getComponent(i) instanceof AbstractButton) {
+            ((AbstractButton) cbClass.getComponent(i)).setBorderPainted(false);
+        }
+    }
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/concluir.png"))); // NOI18N
-        jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 0));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 390, 320, 40));
+    jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+    jLabel3.setText("Classe do Personagem");
+    getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, -1, -1));
 
-        jPanel1.setBackground(new java.awt.Color(111, 111, 95));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+    jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/concluir.png"))); // NOI18N
+    jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 0));
+    jButton1.addActionListener(new java.awt.event.ActionListener() {
+        public void actionPerformed(java.awt.event.ActionEvent evt) {
+            jButton1ActionPerformed(evt);
+        }
+    });
+    getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 390, 320, 40));
 
-        characterGif.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gifs/archer.gif"))); // NOI18N
-        jPanel1.add(characterGif, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, -1, -1));
+    jPanel1.setBackground(new java.awt.Color(111, 111, 95));
+    jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        characterName.setBackground(new java.awt.Color(0, 0, 0));
-        characterName.setForeground(new java.awt.Color(255, 255, 255));
-        characterName.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        characterName.setMaximumSize(new java.awt.Dimension(0, 80));
-        characterName.setName(""); // NOI18N
-        jPanel1.add(characterName, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, 80, -1));
+    characterGif.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gifs/archer.gif"))); // NOI18N
+    jPanel1.add(characterGif, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, -1, -1));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 40, 140, 140));
+    characterName.setBackground(new java.awt.Color(0, 0, 0));
+    characterName.setForeground(new java.awt.Color(255, 255, 255));
+    characterName.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+    characterName.setMaximumSize(new java.awt.Dimension(0, 80));
+    characterName.setName(""); // NOI18N
+    jPanel1.add(characterName, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, 80, -1));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/backblur.png"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 580, 490));
+    getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 40, 140, 140));
 
-        pack();
+    jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/backblur.png"))); // NOI18N
+    jLabel1.setText("jLabel1");
+    getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 580, 490));
+
+    pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void cbClassActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbClassActionPerformed
         // TODO add your handling code here:
         int i = cbClass.getSelectedIndex();
-        
+
         switch (i) {
             case 0:
                 specialField.setText("Destreza");
-                
+
                 hpInput.setText("60");
                 speedInput.setText("15");
                 specialInput.setText("5");
-                
+
                 characterGif.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gifs/archer.gif")));
                 break;
             case 1:
                 specialField.setText("Força");
-                
+
                 hpInput.setText("70");
                 speedInput.setText("10");
                 specialInput.setText("5");
-                
+
                 characterGif.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gifs/berserker.gif")));
                 break;
             case 2:
                 specialField.setText("Inteligência");
-                
+
                 hpInput.setText("50");
                 speedInput.setText("10");
                 specialInput.setText("5");
-                
+
                 characterGif.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gifs/mage.gif")));
-                break;  
+                break;
             default:
                 throw new AssertionError();
         }
     }//GEN-LAST:event_cbClassActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        if(nameInput.getText().length() >= 4){
+        if (nameInput.getText().length() >= 4) {
             Character p;
-            if (cbClass.getSelectedItem().toString().equals("Berserker")){
+            if (cbClass.getSelectedItem().toString().equals("Berserker")) {
                 Berserker berserker = new Berserker(nameInput.getText(), 70, 10, 5, 10);
                 Context.setSession(berserker);
-            }else if(cbClass.getSelectedItem().toString().equals("Mago")){
+            } else if (cbClass.getSelectedItem().toString().equals("Mago")) {
                 Mage mago = new Mage(nameInput.getText(), 50, 10, 100, 5);
                 Context.setSession(mago);
-            }else{
+            } else {
                 Archer arqueiro = new Archer(nameInput.getText(), 60, 15, 5);
                 Context.setSession(arqueiro);
             }
@@ -216,10 +268,10 @@ public class CharCreation extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Personagem " + p.getName() + " da classe " + cbClass.getSelectedItem().toString() + " foi criado com sucesso!");
             dispose();
             Context.getMainWindow().isThereASession();
-        }else{
+        } else {
             JOptionPane.showMessageDialog(this, "O nome do personagem precisa ter quatro letras ou mais!");
         }
-    
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void nameInputCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_nameInputCaretUpdate
