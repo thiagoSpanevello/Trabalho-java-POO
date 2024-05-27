6adea5cc61291f2ba9739241671ce4191d5a4762
ImaKrp <juliocargoncalves@gmail.com>
2024-05-27 19:46:47 -0300

    Ant

--- src/com/ciscos/project/Ciscos.java
+++ src/com/ciscos/project/Ciscos.java
@@ -1,12 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package com.ciscos.project;
 
 import com.ciscos.project.frames.Principal;
 
+/**
+ *
+ * @author imakrp
+ */
 public class Ciscos {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         Principal principal = new Principal();
         principal.setVisible(true);
         principal.setDefaultCloseOperation(Principal.EXIT_ON_CLOSE);
     }
+    
 }
